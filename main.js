25541bdfec4d336c03c90022a27aac83dd8a2455
Shreya-coder-25 <79853835+Shreya-coder-25@users.noreply.github.com>
2021-07-22 19:43:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+nose_x=0;
+nose_y=0;
 function preload()
 {
-
+    mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
 }
 function setup()
 {
@@ -8,13 +10,15 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.size(300,300);
-    video.hide();
-    poseNet= ml5.poseNet(video,modelLoaded);
+    video.hide()
+    poseNet=ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
+    
 }
 function draw()
 {
     image(video,0,0,300,300);
+    image(mustache, nose_x, nose_y,100,40);
 }
 function take_snapshot()
 {
@@ -24,13 +28,13 @@ function modelLoaded()
 {
     console.log("PoseNet is Initialized");
 }
- function gotPoses(results)
- {
-     if(results.length > 0)
-     {
-         console.log(results)
-         console.log("nose x= "+results[0].pose.nose.x-10);
-         console.log("nose y= "+results[0].pose.nose.y-10);
-     }
- }
-
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        nose_x= +results[0].pose.nose.x-50;
+        nose_y= +results[0].pose.nose.y;
+    }
+    
+}
